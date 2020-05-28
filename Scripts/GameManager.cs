f4b8b1e3646dcd5956d5344868c604e00e1c1c0b
indiankid <rshar009@ucr.edu>
2020-05-27 21:48:52 -0700

    Game Manager
    - controls replay button
    -needs pause/options

---
+++ Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject replayButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1;
+    }
+
+    // Update is called once per frame
+    public void GameOver()
+    {
+        StartCoroutine(ExampleCoroutine());
+        //Time.timeScale = 0;
+        //replayButton.SetActive(true);
+    }
+    IEnumerator ExampleCoroutine()
+    {
+        //yield on a new YieldInstruction that waits for 5 seconds.
+        yield return new WaitForSeconds(1);
+        Time.timeScale = 0;
+        replayButton.SetActive(true);
+    }
+    public void OnReplayButtonPressed()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+}
